3da490babca9d44a8029e0d852fd478906050429
Consoli310 <matheusconsoli310@gmail.com>
2024-09-16 16:31:43 -0300

    Subtotal & Total methods

--- src/main/java/com/consoli/course/entities/Order.java
+++ src/main/java/com/consoli/course/entities/Order.java
@@ -89,6 +89,14 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public double getTotal(){
+        double sum = 0;
+        for (OrderItem x : items){
+            sum = sum + x.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
